functional maml structure, need to verify gradients and algo is correct

grasp_env.py
@@ -66,8 +66,9 @@ class GraspEnv(gym.Env):
 
         if action_size is None:
             action_size = self.env.action_size
+        self.action_size = action_size
         self.action_space = spaces.Box(
-            low=-1.0, high=1.0, shape=(action_size,))
+            low=-1.0, high=1.0, shape=(self.action_size,))
 
         if observation_mode == 'state':
             self.observation_space = spaces.Box(
@@ -143,6 +144,18 @@ class GraspEnv(gym.Env):
         if mode == 'rgb_array':
             return self._gym_cam.capture_rgb()
 
+    def switch_task(self, task_class):
+        self.task = self.env.get_task(task_class)
+
+    @staticmethod
+    def switch_task_wrapper(self, task_class: rlbench.backend.task.Task):
+        """Change current task by specifying desired task class. Task objects are randomly initialized.
+
+        Args:
+            task_class (rlbench.backend.task.Task): desired task class
+        """
+        self.envs[0].switch_task(task_class)
+
     def reset(self) -> Dict[str, np.ndarray]:
         descriptions, obs = self.task.reset()
         print("RESET!")
@@ -164,13 +177,14 @@ class GraspEnv(gym.Env):
 
         d_pos = np.array([ax, ay, az])
         d_pos /= (np.linalg.norm(d_pos) * 100.0)
-        # d_quat = np.array([0, 0, 0, 1.0])
+        d_quat = np.array([0, 0, 0, 1.0])
         d_euler = action[3:6] / 10.0
         drho, dphi, dtheta = d_euler
         rot = R.from_euler("xyz", [drho, dphi, dtheta], degrees=True)
         d_quat = rot.as_quat()
 
         gripper_open = action[-1]
+        # gripper_open = 1.0
 
         if self.task._action_mode.arm in self.delta_ee_control_types:
             action = np.concatenate([d_pos, d_quat, [gripper_open]])

reach_task.py
@@ -1,3 +1,11 @@
+from typing import List, Tuple
+import numpy as np
+from pyrep.objects.shape import Shape
+from pyrep.objects.proximity_sensor import ProximitySensor
+from rlbench.const import colors
+from rlbench.backend.task import Task
+from rlbench.backend.spawn_boundary import SpawnBoundary
+from rlbench.backend.conditions import DetectedCondition
 from rlbench.tasks import ReachTarget
 import numpy as np
 
@@ -5,7 +13,7 @@ import numpy as np
 class ReachTargetCustom(ReachTarget):
 
     def reward(self) -> float:
-    
+
         dist = self.target.get_position(self.robot.arm.get_tip())
         dist_val = np.linalg.norm(dist) / 10.0
 #         if dist_val < 0.1:
@@ -14,3 +22,59 @@ class ReachTargetCustom(ReachTarget):
 
     def get_name(self):
         return "reach_target"
+
+
+class ReachTargetCustomStatic(ReachTarget):
+
+    def reward(self) -> float:
+
+        dist = self.target.get_position(self.robot.arm.get_tip())
+        dist_val = np.linalg.norm(dist) / 10.0
+#         if dist_val < 0.1:
+#             return 1
+        return -dist_val
+
+    def get_name(self):
+        return "reach_target"
+
+    def init_task(self) -> None:
+
+        super().init_task()
+        # set object poses only once in the beginning
+        b = SpawnBoundary([self.boundaries])
+        self.static_positions = []
+        for ob in [self.target, self.distractor0, self.distractor1]:
+            b.sample(ob, min_distance=0.2,
+                     min_rotation=(0, 0, 0), max_rotation=(0, 0, 0))
+            self.static_positions.append(ob.get_position())
+
+        print("static positions:")
+        for v in self.static_positions:
+            print(v)
+
+        import ipdb
+        ipdb.set_trace()
+
+    def init_episode(self, index: int) -> List[str]:
+
+        color_name, color_rgb = colors[index]
+        self.target.set_color(color_rgb)
+        print("static positions:")
+        for v in self.static_positions:
+            print(v)
+        for i, ob in enumerate([self.target, self.distractor0, self.distractor1]):
+            ob.set_position(self.static_positions[i])
+
+        # randomize the distractor object colors
+        # color_choices = np.random.choice(
+        #     list(range(index)) + list(range(index + 1, len(colors))),
+        #     size=2, replace=False)
+        # for ob, i in zip([self.distractor0, self.distractor1], color_choices):
+        #     name, rgb = colors[i]
+        #     ob.set_color(rgb)
+
+        # randomize pose of objects
+        # b = SpawnBoundary([self.boundaries])
+        # for ob in [self.target, self.distractor0, self.distractor1]:
+        #     b.sample(ob, min_distance=0.2,
+        #              min_rotation=(0, 0, 0), max_rotation=(0, 0, 0))