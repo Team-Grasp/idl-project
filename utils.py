abfac1e11459806074ba9250d9012804ca46c3bc
Alvin Shek <ashek@andrew.cmu.edu>
2020-12-05 23:54:37 -0500

    add utils

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
@@ -10,7 +11,11 @@ def parse_arguments():
                         default=3e-4, help="The learning rate.")
 
     parser.add_argument('--model_path', dest='model_path', type=str,
-                    default="", help="Existing model to use.")
+                        default="", help="Existing model to use.")
+    parser.add_argument('--train_targets_path', dest='train_targets_path', type=str,
+                        default="", help="Existing train_targets to use.")
+    parser.add_argument('--test_targets_path', dest='test_targets_path', type=str,
+                        default="", help="Existing test_targets to use.")
 
     # Mode
     parser_group = parser.add_mutually_exclusive_group(required=False)
@@ -23,11 +28,11 @@ def parse_arguments():
     parser.set_defaults(render=False)
 
     parser.add_argument('--train', dest='train',
-                              action='store_true',
-                              help="Whether to train model.")
+                        action='store_true',
+                        help="Whether to train model.")
     parser.add_argument('--eval', dest='train',
-                              action='store_false',
-                              help="Whether to evaluate an existing model.")
+                        action='store_false',
+                        help="Whether to evaluate an existing model.")
     parser.set_defaults(train=False)
 
     return parser.parse_args()
